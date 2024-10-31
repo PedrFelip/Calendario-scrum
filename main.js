f7f91c058ed0386f950edc0967ac234fe0daa46c
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2024-10-31 16:59:55 -0300

    Alteração no tamanho da tela que é gerada

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
   // cria uma janela
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       contextIsolation: true, // isolar o contexto
     },
